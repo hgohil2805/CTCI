5102ede705afcd228e229b9b9f7c7fb814a3594c
Hardik Gohil <hcg235@nyu.edu>
2015-09-13 12:49:15 -0400

    Chapter Structure, 1.2

---
+++ CTCI/src/chapter1/ReverseString.java
@@ -0,0 +1,28 @@
+package chapter1;
+import java.util.*;
+public class ReverseString 
+{
+	static Scanner in = new Scanner(System.in);
+	public static String reverseMethod(String s)
+	{
+		if(s.length() <= 1)
+			return s;
+		char[] a = s.toCharArray();
+		int start = 0;
+		int end = a.length - 1;
+		while(start < end)
+		{
+			char temp = a[start];
+			a[start++] = a[end];
+			a[end--] = temp;
+		}
+		return new String(a);
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter the string to be reversed");
+		String inputString = in.nextLine();
+		System.out.println(reverseMethod(inputString));
+	}
+
+}
