a0ae33158b7f521a39461dfbaf188fc30c5fb4ba
hg71335 <hcg235@nyu.edu>
2016-04-11 16:04:35 -0400

    - Class file, cannot continue coz of the fucking lag

---
+++ CTCI/src/LeetCode/Anagrams.java
@@ -0,0 +1,13 @@
+package LeetCode;
+
+/**
+ * Created by hg71335 on 4/11/2016.
+ */
+public class Anagrams {
+    public boolean isAnagram(String s, String t){
+        if(s.length() != t.length())
+            return false;
+
+        return false;
+    }
+}
--- CTCI/src/testFile.java
+++ CTCI/src/testFile.java
@@ -4,6 +4,5 @@ public class testFile {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello to all");
-
 	}
 }
