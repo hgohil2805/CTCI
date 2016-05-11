1410c74005f012debaf0a40a26f913db5c0675a5
Hardik Gohil <hcg235@nyu.edu>
2016-05-10 20:23:38 -0400

    Reverse Vowels in a string - Write a function that takes a string as input and reverse only the vowels of a string.

---
+++ CTCI/src/LeetCode/ReverseVowels.java
@@ -0,0 +1,46 @@
+package LeetCode;
+
+/**
+ * Created by iceman on 5/10/2016.
+ */
+public class ReverseVowels {
+
+    public static void main(String args[]){
+        ReverseVowels obj = new ReverseVowels();
+        System.out.println(obj.reverseVowels("hello"));
+        System.out.println(obj.reverseVowels("leetcode"));
+    }
+
+    public String reverseVowels(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        char[] charString = s.toCharArray();
+        while(i < j){
+            if(this.isVowel(s.charAt(i)) && this.isVowel(s.charAt(j))){
+                char temp = charString[i];
+                charString[i] = charString[j];
+                charString[j] = temp;
+                i++;
+                j--;
+            }
+            else if(this.isVowel(s.charAt(i)) && !this.isVowel(s.charAt(j))){
+                j--;
+            }
+            else if(this.isVowel(s.charAt(j)) && !this.isVowel(s.charAt(i))){
+                i++;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return new String(charString);
+    }
+
+    public boolean isVowel(char a){
+        if(a == 'A'|| a == 'I' || a == 'O' || a == 'E' || a == 'U'
+                || a == 'a'|| a == 'e' || a == 'i' || a == 'o' || a == 'u')
+            return true;
+        return false;
+    }
+}
--- README.md
+++ README.md
@@ -34,3 +34,4 @@
   Say you have an array for which the ith element is the price of a given stock on day i.If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 - Merge two lists
   LinkedList.mergeTwoLists class
+- Reverse vowels in a string
