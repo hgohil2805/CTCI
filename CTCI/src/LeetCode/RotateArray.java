bee2a45278e33ee77ff0a9a236bbaa4f79fa60b5
Hardik Gohil <hcg235@nyu.edu>
2016-08-15 12:46:57 -0400

    Rotate an array of n elements to the right by k steps

---
+++ CTCI/src/LeetCode/RotateArray.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+/**
+ * Created by iceman on 8/15/2016.
+ */
+public class RotateArray {
+    public static void main(String args[]){
+        RotateArray obj = new RotateArray();
+        int[] test = {1,2,3,4,5,6,7};
+        obj.rotate(test,3);
+        System.out.println(Arrays.toString(test));
+    }
+
+
+    public void rotate(int[] nums, int k) {
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end){
+        while(start < end ){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+}
